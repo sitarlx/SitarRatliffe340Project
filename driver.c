e84585f61c9b365ee402182cd207c1bb9f6297c7
Logan <sitarlx@g.cofc.edu>
2020-11-15 17:35:44 -0500

    created Makefile

--- driver.c
+++ driver.c
@@ -1,1 +1,13 @@
-// this is driver
+// driver class
+
+int main(int argc, char** argv) {
+	if (argc != 2){
+		printf("Usage: ./driver <arg>\n");
+		return 1;
+	}
+    	else {}
+    	
+    	// loop for main mem access
+    	
+    	// loop for cache access
+}
