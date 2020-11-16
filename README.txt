9133e0a2b09a1817e73b4ff057f15125779c955c
Logan <sitarlx@g.cofc.edu>
2020-11-15 19:17:50 -0500

    few edits

---
+++ README.txt
@@ -0,0 +1,7 @@
+SitarRatliffe340Project
+
+Questions to Asnswer:
+1. How big is a cache block?
+2. How big is the cache?
+3. How long does a reference to main memory take to complete?
+4. How long does a reference that can be satisfied from cache take to complete?
--- driver.c
+++ driver.c
@@ -8,6 +8,16 @@ int main(int argc, char** argv) {
     	else {}
     	
     	// loop for main mem access
+    	for (int i = 0; i < 10; i++) {}
     	
     	// loop for cache access
+    	for (int j = 0; j < 10; j++) {}
 }
+
+// cache block size function
+
+// cache size function
+
+// main memory access time function
+
+// cache memory access time function
