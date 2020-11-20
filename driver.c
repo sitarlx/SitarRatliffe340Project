d5c64f3921ff10bc7fc385ad834985149589120b
Logan <sitarlx@g.cofc.edu>
2020-11-20 16:22:10 -0500

    modified driver

--- driver.c
+++ driver.c
@@ -1,17 +1,45 @@
 // driver class
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//To compile program...
+//	gcc -Wall -o test driver.c
+//To run program...
+//	./test <int arg1>
+
+
+
+struct timespec start, stop;
+double elapsed;
+int step = 8;
 
 int main(int argc, char** argv) {
 	if (argc != 2){
-		printf("Usage: ./driver <arg>\n");
+		printf("Usage: ./test <arraySize>\n");
 		return 1;
 	}
-    	else {}
-    	
-    	// loop for main mem access
-    	for (int i = 0; i < 10; i++) {}
-    	
-    	// loop for cache access
-    	for (int j = 0; j < 10; j++) {}
+    	else {
+	    	long aSize = atoi(argv[1]);
+	    	printf("aSize = %lu\n", aSize);
+	    	int n[aSize];
+	    	
+	    	// loop for main mem access
+	    	for (int i = 1; i < step; i++) {
+	   		//take time here
+	   		clock_gettime(CLOCK_REALTIME, &start);
+	    		for (int j = 0; j < aSize; j++) {
+	    		}
+	    		//take time here
+	   		clock_gettime(CLOCK_REALTIME, &stop);
+	    	}
+	    	elapsed = stop.tv_sec - start.tv_sec;
+	    	elapsed += (stop.tv_nsec - start.tv_nsec) / 1000000000.0;
+		printf("Access time %.1fms\n", elapsed * 1000);   
+	}
+	
+    	return 0;
+
 }
 
 // cache block size function
