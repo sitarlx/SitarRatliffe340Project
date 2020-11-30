4f79a399d50d8a1f2fd6e82f23d0e375aed8ff27
Logan <sitarlx@g.cofc.edu>
2020-11-30 13:12:00 -0500

    driver.c changes

--- driver.c
+++ driver.c
@@ -20,25 +20,33 @@ int main(int argc, char** argv) {
 		return 1;
 	}
     	else {
+			FILE *fp = fopen("Report.txt", "w");
 	    	long aSize = atoi(argv[1]);
 	    	printf("aSize = %lu\n", aSize);
 	    	int n[aSize];
+			
+			//populate array
+	    	for (int t = 0; t < aSize; t++) {
+	    		n[t] = 420;
+	    	}
 	    	
 	    	// loop for main mem access
 	    	for (int i = 1; i < step; i++) {
-	   		//take time here
-	   		clock_gettime(CLOCK_REALTIME, &start);
+				// take time here
+				clock_gettime(CLOCK_REALTIME, &start);
 	    		for (int j = 0; j < aSize; j++) {
+					// acess array here
 	    		}
-	    		//take time here
-	   		clock_gettime(CLOCK_REALTIME, &stop);
+	    		// take time here
+				clock_gettime(CLOCK_REALTIME, &stop);
+				elapsed = stop.tv_sec - start.tv_sec;
+				elapsed += (stop.tv_nsec - start.tv_nsec) / 1000000000.0;
+				fprintf(fp, "Access time for i = %d is %.1fms\n", i, elapsed * 1000);
 	    	}
-	    	elapsed = stop.tv_sec - start.tv_sec;
-	    	elapsed += (stop.tv_nsec - start.tv_nsec) / 1000000000.0;
-		printf("Access time %.1fms\n", elapsed * 1000);   
+		fclose(fp);	   
 	}
 	
-    	return 0;
+    return 0;
 
 }
 
