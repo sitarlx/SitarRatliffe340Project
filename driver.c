6a785aea29ba2906f00bdaf7a58a8f710c816ab9
Logan <sitarlx@g.cofc.edu>
2020-12-02 12:33:07 -0500

    format cahnges

--- driver.c
+++ driver.c
@@ -6,52 +6,52 @@
 //To compile program...
 //	gcc -Wall -o test driver.c
 //To run program...
-//	./test <int arg1>
+//	./test <int arg1> <int arg2>
 
 int main(int argc, char** argv) {
+	
+	// check correct num args
 	if (argc != 3){
 		printf("Usage: ./test <arraySize> <stepSize>\n");
 		return 1;
 	}
-    	else {
+    else {
 		FILE *fp = fopen("Report.txt", "w");
-	    	long aSize = atol(argv[1]);
-	    	long max_step = atol(argv[2]);
-	    	printf("aSize = %lu\n", aSize);
+	    long aSize = atol(argv[1]);
+	    long max_step = atol(argv[2]);
+	    printf("aSize = %lu\n", aSize);
 	    	
-	    	struct timespec start, stop;
-	    	long x;
-	    	double elapsed;
+	    struct timespec start, stop;
+	    long x;
+	    double elapsed;
 			
-		//populate array
-	    	
-	    	// loop for main mem access
-	    	for (long step = 1; step <= max_step; step++) {
-	    		x = 0;
-	    		long bSize = aSize * step;
-	    		long *n = (long*)malloc(bSize*sizeof(long));
+
+	    // loop for main mem access
+	    for (long step = 1; step <= max_step; step++) {
+			x = 0;
+	    	long bSize = aSize * step;
+	    	long *n = (long*)malloc(bSize*sizeof(long));
 	    		
-	    		for (long t = 0; t < bSize; t++) {
-	    			n[t] = rand();
-	    		}
+			//populate array
+	    	for (long t = 0; t < bSize; t++) {
+	    		n[t] = rand();
+	    	}
 			// take time here
 			clock_gettime(CLOCK_MONOTONIC, &start);
-	    		for (long j = 0; j < bSize; j+=step) {
-				x = x + n[j];
-	    		}
+	    	for (long j = 0; j < bSize; j+=step) {
+			x = x + n[j];
+	    	}
 	    		
-	    		// take time here
+	    	// take time here
 			clock_gettime(CLOCK_MONOTONIC, &stop);
 			elapsed = stop.tv_nsec - start.tv_nsec;
 			elapsed += (stop.tv_sec - start.tv_sec) * 1000000000.0;
 			fprintf(fp, "Mean access time for step = %ld is %.1lfns. With value %ld\n", step, elapsed/aSize, x);
 			free(n);
-	    	}
+	    }
 		fclose(fp);	   
 	}
-	
     return 0;
-
 }
 
 // cache block size function
