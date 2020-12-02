758176178846eed2c9d4e2ece99c63c962198a97
ryan ratliffe <ryanoliverratliffe@gmail.com>
2020-12-02 15:13:28 -0500

    3/4 wuestions

--- driver.c
+++ driver.c
@@ -7,7 +7,7 @@
 //	gcc -Wall -o test driver.c
 //To run program...
 //	./test <int arg1> <int arg2>
-
+long max_its = 10000;
 int main(int argc, char** argv) {
 	
 	// check correct num args
@@ -15,43 +15,54 @@ int main(int argc, char** argv) {
 		printf("Usage: ./test <arraySize> <stepSize>\n");
 		return 1;
 	}
-    else {
+	else {
 		FILE *fp = fopen("Report.txt", "w");
-	    long aSize = atol(argv[1]);
-	    long max_step = atol(argv[2]);
-	    printf("aSize = %lu\n", aSize);
-	    	
-	    struct timespec start, stop;
-	    long x;
-	    double elapsed;
-			
-
-	    // loop for main mem access
-	    for (long step = 1; step <= max_step; step++) {
-			x = 0;
-	    	long bSize = aSize * step;
-	    	long *n = (long*)malloc(bSize*sizeof(long));
-	    		
+		long aSize = atol(argv[1]);
+		long max_step = atol(argv[2]);
+		double m[max_step];
+		struct timespec start, stop;
+	    	long x;
+	  	double elapsed;
+	  	  
+		double sum_x = 0;	
+		printf("size of long = %ld\n", sizeof(long));
+	  	 // loop for main mem access
+	   	 for (long step = 1; step <= max_step; step++) {
+	    		long bSize = aSize * step;
 			//populate array
-	    	for (long t = 0; t < bSize; t++) {
-	    		n[t] = rand();
-	    	}
-			// take time here
-			clock_gettime(CLOCK_MONOTONIC, &start);
-	    	for (long j = 0; j < bSize; j+=step) {
-			x = x + n[j];
-	    	}
+	    		m[step-1] = 0;
 	    		
-	    	// take time here
-			clock_gettime(CLOCK_MONOTONIC, &stop);
-			elapsed = stop.tv_nsec - start.tv_nsec;
-			elapsed += (stop.tv_sec - start.tv_sec) * 1000000000.0;
-			fprintf(fp, "Mean access time for step = %ld is %.1lfns. With value %ld\n", step, elapsed/aSize, x);
-			free(n);
-	    }
-		fclose(fp);	   
+			for (long k = 0; k < max_its; k++) {
+				long *n = (long*)malloc(bSize*sizeof(long));
+				x = 0;
+				//for (long t = 0; t < bSize; t++) {
+	    				//n[t] = rand();
+	    			//}
+	    			
+				// take time here
+		    		clock_gettime(CLOCK_MONOTONIC, &start);
+		    		for (long j = 0; j < bSize; j+=step) {	
+					x = x + n[j];	
+		    		}
+		    		// take time here
+				clock_gettime(CLOCK_MONOTONIC, &stop);
+				elapsed = stop.tv_nsec - start.tv_nsec;
+				elapsed += (stop.tv_sec - start.tv_sec) * 1000000000.0;
+				m[step - 1] += (elapsed/aSize);
+	    			
+				sum_x +=x;
+				free(n);
+			} //end of k loop
+			
+	  	} // end of step loop
+	  	for(long z = 1;z<max_step;z++){
+	  		m[z-1] = m[z-1]/max_its;
+	  		fprintf(fp, "Mean access time for step = %ld bytes is %.1lfns. \n", z*sizeof(long), m[z-1]);
+	  	}
+		fclose(fp);	  
+		printf("this is the sum of x: %lf", sum_x);
 	}
-    return 0;
+    	return 0;
 }
 
 // cache block size function
